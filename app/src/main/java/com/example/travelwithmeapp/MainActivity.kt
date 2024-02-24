50f119c9310798a65fcad74b3fa19a3a24137cf7
Samuel Fraisoli <samuelfraisoli@gmail.com>
2024-02-24 15:45:27 +0100

    añadido viewBinding

--- app/src/main/java/com/example/travelwithmeapp/MainActivity.kt
+++ app/src/main/java/com/example/travelwithmeapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.travelwithmeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.travelwithmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        val view: View = binding.root
+        setContentView(view)
     }
 }
 
