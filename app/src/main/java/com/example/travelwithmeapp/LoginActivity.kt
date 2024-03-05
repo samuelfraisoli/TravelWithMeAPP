eb516790821d0aefca956ddaa66ae071c4405aa4
Matías <matiasmartinezmartinez318@gmail.com>
2024-03-05 21:33:32 +0100

    Ventana login activity parte grafica y creación de la activity registrarse

---
+++ app/src/main/java/com/example/travelwithmeapp/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.travelwithmeapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.travelwithmeapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
